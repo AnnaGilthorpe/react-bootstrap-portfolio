b91bb66a7a631a08066a4cb4aa0a6f5c278ade87
Anna Gilthorpe <annagilthorpe@gmail.com>
2023-03-13 11:02:54 +0000

    hero image added

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+        <div className="main-info">
+            <h1>Anna Gilthorpe // Website Developer</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
